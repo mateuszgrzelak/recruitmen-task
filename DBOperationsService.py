e1574926d85673c0c6b460d19583913b0196e7f8
Mategrze <mgrzelak1997@wp.pl>
2020-08-03 23:11:43 +0200

    Average age

--- DBOperationsService.py
+++ DBOperationsService.py
@@ -20,3 +20,20 @@ class DBOperationsService:
         self.cur.execute('SELECT COUNT(*) FROM person WHERE gender="female"')
         number_woman = self.cur.fetchall()[0][0]
         print('Procent mezczyzn = ' + str(number_man / (number_man + number_woman) * 100) + '%')
+
+    def average_age(self):
+        self.cur.execute('SELECT AVG(age) FROM personDOB')
+        average_age_overall = self.cur.fetchall()[0][0]
+        print('Ogolna srednia wieku: ' + str(int(average_age_overall)))
+
+    def average_age_male(self):
+        self.cur.execute('SELECT AVG(age) FROM personDOB,'
+                         ' person WHERE person.gender="male" AND person.id=personDOB.id')
+        average_age_man = self.cur.fetchall()[0][0]
+        print('Srednia wieku mezczyzn: ' + str(int(average_age_man)))
+
+    def average_age_female(self):
+        self.cur.execute('SELECT AVG(age) FROM personDOB,'
+                         ' person WHERE person.gender="female" AND person.id=personDOB.id')
+        average_age_woman = self.cur.fetchall()[0][0]
+        print('Srednia wieku kobiet: ' + str(int(average_age_woman)))
