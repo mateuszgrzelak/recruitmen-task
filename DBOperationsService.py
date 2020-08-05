6c91cce87996cf903a233d6828155900a37f06f2
Mategrze <mgrzelak1997@wp.pl>
2020-08-05 11:58:07 +0200

    Code refactoring

--- DBOperationsService.py
+++ DBOperationsService.py
@@ -7,62 +7,65 @@ class DBOperationsService:
         self.__conn = sqlite3.connect(db_name)
         self.__cur = self.__conn.cursor()
 
-    def __get_number_man_woman(self):
+    def __get_number_men_women(self) -> (int, int):
         self.__cur.execute('SELECT COUNT(*) FROM person WHERE gender="male"')
-        number_man = self.__cur.fetchall()[0][0]
+        number_men = self.__cur.fetchall()[0][0]
         self.__cur.execute('SELECT COUNT(*) FROM person WHERE gender="female"')
-        number_woman = self.__cur.fetchall()[0][0]
-        return number_man, number_woman
+        number_women = self.__cur.fetchall()[0][0]
+        return number_men, number_women
 
-    def percentage_women(self):
-        number_man, number_woman = self.__get_number_man_woman()
-        return number_woman / (number_man + number_woman) * 100
+    def percentage_women(self) -> int:
+        number_men, number_women = self.__get_number_men_women()
+        return number_women / (number_men + number_women) * 100
 
-    def percentage_man(self):
-        number_man, number_woman = self.__get_number_man_woman()
-        return number_woman / (number_man + number_woman) * 100
+    def percentage_men(self) -> int:
+        number_men, number_women = self.__get_number_men_women()
+        return number_men / (number_men + number_women) * 100
 
-    def average_age_generally(self):
+    def average_age_generally(self) -> int:
         self.__cur.execute('SELECT AVG(age) FROM personDOB')
         average_age = self.__cur.fetchall()[0][0]
         return average_age
 
-    def average_age_male(self):
+    def average_age_male(self) -> int:
         self.__cur.execute('SELECT AVG(age) FROM personDOB,'
                            ' person WHERE person.gender="male" AND person.id=personDOB.id')
-        average_age_man = self.__cur.fetchall()[0][0]
-        return average_age_man
+        average_age_men = self.__cur.fetchall()[0][0]
+        return average_age_men
 
-    def average_age_female(self):
+    def average_age_female(self) -> int:
         self.__cur.execute('SELECT AVG(age) FROM personDOB,'
                            ' person WHERE person.gender="female" AND person.id=personDOB.id')
-        average_age_woman = self.__cur.fetchall()[0][0]
-        return average_age_woman
+        average_age_women = self.__cur.fetchall()[0][0]
+        return average_age_women
 
-    def most_popular_cities(self, limit: int):
+    def most_common_cities(self, limit: int) -> list:
         self.__cur.execute('SELECT city, COUNT(city) as cnt FROM personLocation'
                            ' GROUP BY city order by cnt desc LIMIT ' + str(limit))
         result = self.__cur.fetchall()
-        return result[0]
+        return result
 
-    def most_common_passwords(self, limit: int):
+    def most_common_passwords(self, limit: int) -> list:
         self.__cur.execute('SELECT password, COUNT(password) as cnt FROM personLogin'
                            ' GROUP BY password order by cnt desc LIMIT ' + str(limit))
         result = self.__cur.fetchall()
-        return result[0]
+        return result
 
-        # d1 and d2 format is YYYY-MM-DD
-
-    def users_born_between_dates(self, d1, d2):
+    def users_born_between_dates(self, d1: str, d2: str) -> list:
+        '''
+        :param d1: # format YYYY-MM-DD
+        :param d2: # format YYYY-MM-DD
+        :return:
+        '''
         # first date is inclusive, but the second is exclusive, i.e. d2 is equal YYYY:MM:DD:00:00:00
         # to prevent from being out of scope added following modification
         d2buff = d2 + 'T23:59:59.999Z'
         self.__cur.execute("SELECT username FROM personLogin, personDOB WHERE personDOB.date >= '" + d1 + "'"
                           " and personDOB.date <= '" + d2buff + "'and personDOB.id = personLogin.id")
         result = self.__cur.fetchall()
-        return result[0]
+        return result
 
-    def __get_points_for_password(self, password):
+    def __get_points_for_password(self, password: str) -> int:
         special_characters = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+",
                               ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[",
                               "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
@@ -88,7 +91,7 @@ class DBOperationsService:
             points += 5
         return points
 
-    def most_secure_password(self):
+    def most_secure_password(self) -> list:
         self.__cur.execute('SELECT password FROM personLogin')
         result = self.__cur.fetchall()
         max_points = 0
